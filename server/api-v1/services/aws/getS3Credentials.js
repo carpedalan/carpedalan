d02f619d4a81862580040e2fe34a7db81b4858eb
Leif Dalan <leifdalan@gmail.com>
2019-10-05 16:27:40 -0700

    Fixed lint and dockerfile

--- server/api-v1/services/aws/getS3Credentials.js
+++ server/api-v1/services/aws/getS3Credentials.js
@@ -25,9 +25,7 @@ function getS3Parameters(config, filename) {
   const dateString = date.substr(0, 4) + date.substr(5, 2) + date.substr(8, 2);
 
   // create upload credentials
-  const credential = `${config.access_key}/${dateString}/${
-    config.region
-  }/s3/aws4_request`;
+  const credential = `${config.access_key}/${dateString}/${config.region}/s3/aws4_request`;
 
   // create policy
   const policy = {
